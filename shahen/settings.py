dd8f57b1e2506d20c2f06c56690d582dcd3ad692
Wesam <wisam.sinbad@gmail.com>
2026-03-31 01:06:12 +0300

    save settings and latest core migration

--- shahen/settings.py
+++ shahen/settings.py
@@ -163,9 +163,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static'
 ]
 
 MEDIA_URL = '/media/'
